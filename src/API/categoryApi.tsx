87f036df82a66c334c46be54edaf54f7190d81c1
TRANHUUTINH <tranhuutjnh@gmail.com>
2025-08-08 06:53:59 +0700

    API

--- src/API/categoryApi.tsx
+++ src/API/categoryApi.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { Category } from "../types/CategoryDTO";
 import { ProductDTO } from "../types/ProductDTO";
 
-const BASE_API_URL = "https://mahai-backend-production.up.railway.app/api";
+const BASE_API_URL = "https://mahai-app-service-production.up.railway.app/api";
+
 
 export const fetchCategories = async (): Promise<Category[]> => {
   const res = await axios.get(`${BASE_API_URL}/categories`);
